aa99cd251daccf1dd0d1b105a457c14713c3098b
Heber Quequejana <heberqc@gmail.com>
2017-12-15 12:01:55 -0500

    avance de analisis

--- tesis.py
+++ tesis.py
@@ -115,4 +115,73 @@ for sec in range(500):
 #arreglo de nuevos grafos generados
 ngg = []
 
+for sec in range(500):
+	#nodos y enlaces temporales
+	nt, et = fg.uda_model(nsg[sec], ["G_diagonal", "G_0"])
+	grafot = nx.Graph()
+	grafot.add_nodes_from(nt)
+	grafot.add_edges_from(et)
+	ngg.append(grafot)
+
+# =============================================================================
+# Análisis de grafos generados
+# =============================================================================
+
+c_enlaces = []
+for sec in range(500):
+	c_enlaces.append(nx.number_of_edges(ngg[sec]))
+
+# =============================================================================
+# prueba caso 0
+# =============================================================================
+
+#prueba de generación de grafos con 1000 nodos
+aux0 = []
+for bla in range(1000):
+	grado = buscarClase(np.random.random())
+	if (grado > tabla['i'][len(tabla) - 1]):
+		aux0.append(cn)
+	else:
+		aux0.append(grado)
+
+naux, eaux = fg.uda_model(aux0, ['G_diagonal', 'G_0'])
+fg.imprimir_gephi(naux, eaux)
+
+gaux0 = nx.Graph()
+gaux0.add_nodes_from(naux)
+gaux0.add_edges_from(eaux)
+nx.density(gaux0)
+
+nx.info(gaux0)
+
+npaux0 = np.array(aux0, dtype='int64')
+
+# =============================================================================
+# prueba caso 1
+# =============================================================================
+
+#prueba de generación de grafos con 1000 nodos
+aux1 = []
+for bla in range(1000):
+	grado = buscarClase(np.random.random())
+	if (grado > tabla['i'][len(tabla) - 1]):
+		aux1.append(cn)
+	else:
+		aux1.append(grado)
+
+naux1, eaux1 = fg.uda_model(aux1, ['G_diagonal', 'G_0', 'G_triangulo'])
+fg.imprimir_gephi(naux1, eaux1)
+
+gaux1 = nx.Graph()
+gaux1.add_nodes_from(naux)
+gaux1.add_edges_from(eaux)
+nx.density(gaux1)
+
+nx.info(gaux1)
+
+npaux1 = np.array(aux1, dtype='int64')
+
+# =============================================================================
+
+npnsg = np.array(nsg, dtype='int64')
 
